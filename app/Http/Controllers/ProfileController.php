fd7690e147a2ee2e3867ae87af28df7d5e0fac30
Scott Anderson <scott@getcodesmart.com>
2021-02-05 15:26:22 -0500

    Added missing controller.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
-    //
+    public function show($username)
+    {
+        if ($username) {
+            $username = User::where('username', '=', $username)->firstOrFail();
+        }
+
+        return $username->get_biography();
+    }
+
+    public function index()
+    {
+        return view('profile');
+    }
 }
